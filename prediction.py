7fbd26f8494f9aac92944eae54f69d283b7307f1
Deepesh1024 <d4deepeshpro@gmail.com>
2025-01-22 00:01:37 +0530

    new pushes

--- prediction.py
+++ prediction.py
@@ -1,14 +1,14 @@
 import numpy as np
 def predictor(sys,dia):
-    gender = 1
-    height = 162
-    weight = 70.6
+    gender = 2
+    height = 167
+    weight = 65
     ap_hi = sys
     ap_low = dia 
-    smoke = 0
-    alco = 0
+    smoke = 1
+    alco = 1
     active = 1
-    age = 40
+    age = 45
     
     para = np.array([[gender, height, weight, ap_hi, ap_low,smoke, alco, active, age]])
     weights = np.array([[4.02893908e-01, -4.58755977e-02 , 1.94876975e-02 , 3.70367864e-02,
